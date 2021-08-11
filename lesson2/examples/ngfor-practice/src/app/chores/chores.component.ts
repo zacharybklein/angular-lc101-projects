0314611cf2e5260086b6d5c6f55d4144756326d9
zacharybklein <zachbklein@gmail.com>
2021-08-11 18:08:43 -0500

    Added changes per book

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+   chores = ['Empty dishwasher', 'Complete LaunchCode prep work'];
+   todoTitles = ["Yesterday's Chores", "Today's Chores"];
 
    constructor() { }
 
