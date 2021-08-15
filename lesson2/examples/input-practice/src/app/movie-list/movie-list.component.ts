ee5717774b0f75c3a3c912396a335a2b6db849b7
zacharybklein <zachbklein@gmail.com>
2021-08-15 10:52:58 -0500

    did tryit practice

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,15 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string) {
+      if (newTitle === "") {
+         alert("Please enter a movie title")
+         return this.movies;
+      }
+      else if (!this.movies.includes(newTitle)) {
+         this.movies.push(newTitle);
+      }
+      
+   }
 }
